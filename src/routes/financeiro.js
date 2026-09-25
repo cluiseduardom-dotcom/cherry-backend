8364f4a71636a54069df09efdce0fd96858b627b
cluiseduardom-dotcom <c.luiseduardom@gmail.com>
2026-09-24 22:54:40 -0300

    feat: expõe fluxo de caixa consolidado

--- src/routes/financeiro.js
+++ src/routes/financeiro.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const pontoEquilibrioController = require('../controllers/pontoEquilibrioController');
+const fluxoCaixaController = require('../controllers/fluxoCaixaController');
 
 router.get('/ponto-equilibrio', pontoEquilibrioController.calcular);
+router.get('/fluxo-caixa', fluxoCaixaController.resumo);
 
 module.exports = router;
